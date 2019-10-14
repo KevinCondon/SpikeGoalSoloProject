891afc798f742c69820c32689704c8aeb8ff17c4
Kevin Condon <kevincondon96@gmail.com>
2019-10-13 20:42:56 -0500

    transition group demo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import SimpleTransition from "./simple-transition.js";
+import './style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    return (
+      <div>
+        <SimpleTransition />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
